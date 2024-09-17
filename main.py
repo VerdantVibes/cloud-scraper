ac93aaa8764856fb113171fbc6a0552f9ff364c6
VerdantVibes <biletskyiivan423@gmail.com>
2024-09-17 08:47:07 +0200

    fix: worker issue

--- main.py
+++ main.py
@@ -1,60 +1,65 @@
 from uc_scraper import WebScraper
 from concurrent.futures import ThreadPoolExecutor
 from cloud_scraper import Cloud_Scraper
-import pandas as pd
 import string
 import queue
 import threading
 import logging
 import tempfile
-import requests
 import time
-import os
 import getpass
+import csv
 
-lock = threading.Lock()
 # Set up logging
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-proxy_url = ""
+
+max_workers = 3
+per_thread = 10  # pages one thread execute
+max_scrapeworkers = 10
+
 data_array = {}
 data_check = {}
+new_data_array = {}
+header = ["url"]
+lock = threading.Lock()
+worker_thread = threading.Lock()
+websitescp_thread = threading.Lock()
 
 
-def create_unique_temp_directory():
-    with lock:
-        driver_temp_dir = tempfile.mkdtemp()
-    return driver_temp_dir
+# Save data from url using Cloud_Scraper
+def process_cloudscraper_url(url, lock):
+    scraper = Cloud_Scraper(url)
+    scraper.run(lock)
+    scraper.close_scraper()
 
 
-per_thread = 10  # pages one thread execute
-proxy_lock = threading.Lock()
-
-
-def worker(task_queue, url, user_data_dir, UserName):
-    with proxy_lock:
-        proxies = {
-            "http": proxy_url,
-            "https": proxy_url,
-        }
-        try:
-            response = requests.get(url, proxies=proxies)
-            print(
-                f"Thread : Using proxy: {proxy_url}, Status code: {response.status_code}"
-            )
-        except requests.RequestException as e:
-            print(f"Thread : Error using proxy: {proxy_url}, Error: {e}")
-        finally:
-            # Simulate proxy rotation delay (if needed)
-            time.sleep(1)  # Sleep for a second to simulate rotation
-
+# Scrape dats from each urls
+def scrape_data(task_queue, lock):
     while not task_queue.empty():
         try:
-            idx, keyword, start_page = task_queue.get_nowait()
+            url = task_queue.get_nowait()
+            process_cloudscraper_url(url, lock)
+            task_queue.task_done()
+        except queue.Empty:
+            break
+        except Exception as e:
+            print(url)
+            logging.error(f"scrape_data Error occurred: {e}")
+
+
+# Scrpe url data
+def worker(task_queue, url, user_data_dir, UserName):
+    with worker_thread:
+        time.sleep(2)
+    while not task_queue.empty():
+        try:
+            _, keyword, start_page = task_queue.get_nowait()
             scraper = WebScraper(user_data_dir, UserName)
-            # logging.info(f"Thread {idx} processing keyword: {keyword}, {start_page}")
-            scraper.scrape_website(url, keyword, data_array, start_page, per_thread)
+            scraper.scrape_website(
+                url, keyword, data_array, start_page, per_thread, websitescp_thread
+            )
             task_queue.task_done()
         except queue.Empty:
             break
@@ -62,49 +67,14 @@ def worker(task_queue, url, user_data_dir, UserName):
             logging.error(f"worker Error occurred: {e}")
 
 
-def save_data_worker(data_array):
-    file_name = "check.xlsx"
-    url_list = list(data_array.keys())
-    urls_to_add = []
-
-    for url in url_list:
-        if data_array.get(url):  # Check if the URL is in data_array and is True
-            if data_check.get(url) == 1:
-                continue
-            else:
-                data_check[url] = 1
-                urls_to_add.append(url)
-    df_new = pd.DataFrame(
-        {
-            "url": urls_to_add,
-            "checked": [True]
-            * len(urls_to_add),  # Assuming all URLs are checked as True
-        }
-    )
-
-    # Check if the file exists
-    if not os.path.exists(file_name):
-        # If the file does not exist, create it with the new data
-        df_new.to_excel(file_name, index=False)
-        print(f"Created new file: {file_name} with initial data.")
-    else:
-        # If the file exists, load the existing Excel file
-        df_existing = pd.read_excel(file_name)
-
-        # Append the new data to the existing DataFrame
-        df_combined = pd.concat([df_existing, df_new], ignore_index=True)
-
-        # Save the combined DataFrame back to the Excel file
-        df_combined.to_excel(file_name, index=False)
-        print(f"Appended data to existing file: {file_name}.")
+# Create unique chromedriver
+def create_unique_temp_directory():
+    with lock:
+        driver_temp_dir = tempfile.mkdtemp()
+    return driver_temp_dir
 
 
-def timed_save_data(data_array):
-    while True:
-        time.sleep(60)  # Wait for 60 seconds
-        save_data_worker(data_array)
-
-
+# Running with thread logic
 def parallel_controller(keywords):
     url = "https://registry.cno.org/Search/Search"
     task_queue = queue.Queue()
@@ -114,13 +84,7 @@ def parallel_controller(keywords):
             task_queue.put((idx, keyword, value))
 
     UserName = getpass.getuser()
-    print(UserName)
-    timed_thread = threading.Thread(
-        target=timed_save_data, args=(data_array,), daemon=True
-    )
-    timed_thread.start()
-    # Use ThreadPoolExecutor to manage parallel execution
-    max_workers = 4
+
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         for i in range(max_workers):
             user_data_dir = create_unique_temp_directory()
@@ -130,11 +94,7 @@ def parallel_controller(keywords):
     task_queue.join()
 
 
-def getUserName():
-    username = os.getlogin()
-    return username
-
-
+# Generate lastname
 def generate_keywords():
     keywords = []
     alphabets = string.ascii_lowercase
@@ -156,51 +116,7 @@ def generate_keywords():
     return keywords
 
 
-# save data using Cloud_Scraper
-def process_cloudscraper_url(url, data_list, lock):
-    scraper = Cloud_Scraper(url)
-    data = scraper.run()
-    scraper.close_scraper()
-
-    with lock:
-        if data:
-            data_list.append(
-                {
-                    "seq": len(data_list) + 1,
-                    "first": data.get("FirstName"),
-                    "last": data.get("LastName"),
-                    "b1": data.get("Category"),
-                    "lic": data.get("Registration Number"),
-                    "status": data.get("Registration Status"),
-                    "addr1": data.get("Address1"),
-                    "city": data.get("City"),
-                    "prov": data.get("Prov"),
-                    "post": data.get("Post"),
-                    "phone": data.get("Phone"),
-                    "extra1_status": data.get("Extra1"),
-                    "extra2_initial_registration": data.get("Start Date"),
-                    "extra3_prescribe_fields": data.get("End Date"),
-                    "extra4": data.get("Class"),
-                    "extra5_permit_issued": data.get("Initial Registration with CNO"),
-                    "extra6": data.get("Specialty Certificate"),
-                    "extra7": data.get("Status"),
-                }
-            )
-
-
-def scrape_data(task_queue, data_list, lock):
-    while not task_queue.empty():
-        try:
-            url = task_queue.get_nowait()
-            process_cloudscraper_url(url, data_list, lock)
-            task_queue.task_done()
-        except queue.Empty:
-            break
-        except Exception as e:
-            print(url)
-            logging.error(f"scrape_data Error occurred: {e}")
-
-
+# Scrape data
 def main():
     # Generate Keywords for Last Name
     keywords = generate_keywords()
@@ -211,76 +127,83 @@ def main():
     # Thread function to parallel webdriver functionality
     parallel_controller(keywords)
 
-    data_list = []
     task_queue = queue.Queue()
     # get detail data from date_array
+    file_name = "check.csv"
+    with open(file_name, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+    print("Saving... Please wait before close")
+
     url_list = list(data_array.keys())
     for url in url_list:
         if data_array[url]:
-            task_queue.put(url)
+            with open(file_name, mode="a", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow([url])
 
+    data_checkurl = {}
+    # Populate the task queue with unique URLs
+    for url in url_list:
+        if data_checkurl.get(url) == 1:
+            continue
+        else:
+            data_checkurl[url] = 1
+            task_queue.put(url)
+    df = [
+        "seq",
+        "last",
+        "first",
+        "suff",
+        "off",
+        "firm",
+        "addr1",
+        "addr2",
+        "city",
+        "prov",
+        "post",
+        "phone",
+        "fax",
+        "fax_p",
+        "ext",
+        "status",
+        "gend",
+        "lic",
+        "lic_p",
+        "f",
+        "b1",
+        "b2",
+        "i1",
+        "i2",
+        "i3",
+        "i4",
+        "email",
+        "email_p",
+        "email_s",
+        "yog",
+        "lang",
+        "prof",
+        "extra1_status",
+        "extra2_initial_registration",
+        "extra3_prescribe_fields",
+        "extra4",
+        "extra5_permit_issued",
+        "extra6",
+        "extra7",
+        "ims",
+        "pref",
+    ]
+    with open("scrape_data.csv", "a", newline="", encoding="utf-8") as nsf:
+        writer = csv.writer(nsf)
+        writer.writerow(df)
     # Create and start 4 scrapingdata threads
-    max_scrapeworkers = 10
+
     scrape_lock = threading.Lock()
     with ThreadPoolExecutor(max_workers=max_scrapeworkers) as executor:
         for i in range(max_scrapeworkers):
-            executor.submit(scrape_data, task_queue, data_list, scrape_lock)
-
-    df = pd.DataFrame(
-        columns=[
-            "seq",
-            "last",
-            "first",
-            "suff",
-            "off",
-            "firm",
-            "addr1",
-            "addr2",
-            "city",
-            "prov",
-            "post",
-            "phone",
-            "fax",
-            "fax_p",
-            "ext",
-            "status",
-            "gend",
-            "lic",
-            "lic_p",
-            "f",
-            "b1",
-            "b2",
-            "i1",
-            "i2",
-            "i3",
-            "i4",
-            "email",
-            "email_p",
-            "email_s",
-            "yog",
-            "lang",
-            "prof",
-            "extra1_status",
-            "extra2_initial_registration",
-            "extra3_prescribe_fields",
-            "extra4",
-            "extra5_permit_issued",
-            "extra6",
-            "extra7",
-            "ims",
-            "pref",
-        ]
-    )
-    for new_data in data_list:
-        try:
-            df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
-        except Exception as e:
-            logging.error(f"new_data Error occurred: {e}")
-    try:
-        df.to_excel("scraped_data.xlsx", index=False)
-    except:
-        logging.error("file save error")
+            executor.submit(scrape_data, task_queue, scrape_lock)
 
 
+# Do main function
 if __name__ == "__main__":
     main()
