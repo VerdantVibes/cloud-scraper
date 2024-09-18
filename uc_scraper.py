fe1ce754adef807aaec7a72e37efbcf115f51b17
VerdantVibes <biletskyiivan423@gmail.com>
2024-09-19 01:51:32 +0200

    feat: refactor uc_scraper

--- uc_scraper.py
+++ uc_scraper.py
@@ -22,24 +22,24 @@ class WebScraper:
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        # chrome_options.add_argument("--incognito")
-        # chrome_options.add_argument("--headless")  # Run headless if you don't need to see the browser
         prefs = {
             "profile.managed_default_content_settings.images": 2,
             "profile.managed_default_content_settings.stylesheets": 2,
         }
         chrome_options.add_experimental_option("prefs", prefs)
-        
+
         chromedriver_path = f"C:\\Users\\{UserName}\\AppData\\Roaming\\undetected_chromedriver\\undetected\\chromedriver-win32\\chromedriver.exe"
 
         if not os.path.isfile(chromedriver_path):
             raise FileNotFoundError(f"Cannot find chromedriver at {chromedriver_path}")
-        
-        chromedriver_temp_path = os.path.join(driver_temp_dir, 'chromedriver.exe')
+
+        chromedriver_temp_path = os.path.join(driver_temp_dir, "chromedriver.exe")
         shutil.copy(chromedriver_path, chromedriver_temp_path)
 
         # Initialize undetected ChromeDriver
-        driver = uc.Chrome(options=chrome_options, driver_executable_path=chromedriver_temp_path)
+        driver = uc.Chrome(
+            options=chrome_options, driver_executable_path=chromedriver_temp_path
+        )
         return driver
 
     # Scrape the website
@@ -53,7 +53,7 @@ class WebScraper:
                 self.driver.get(url)
                 # last_name_input = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#LastName")))
                 time.sleep(5)
-                
+
                 # Find the input field and enter the keyword "aa"
                 last_name_input = self.driver.find_element(By.CSS_SELECTOR, "#LastName")
                 last_name_input.send_keys(lastname)
@@ -66,9 +66,9 @@ class WebScraper:
                 # self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#mainContent > div > div.table-responsive > table")))
                 i = start_page
                 if debugging == 0:
-                    debugging = start_page # for debugging
+                    debugging = start_page  # for debugging
                     time.sleep(8)  # Adjust sleep time as necessary
-                else :
+                else:
                     time.sleep(4)
                 while i < start_page + per_thread:
                     debugging = i
@@ -78,12 +78,16 @@ class WebScraper:
                     time.sleep(3)
                     # Scrape data
                     self.scrape_table(data_array)
-                    
+
                     i = i + 1
-                print(f"----------------Success------------------------{lastname}, {start_page}")
+                print(
+                    f"----------------Success------------------------{lastname}, {start_page}"
+                )
                 break
             except Exception as e:
-                print(f"----------------Retry------------------------ no {attempts} => {lastname}, {start_page}, {debugging}")
+                print(
+                    f"----------------Retry------------------------ no {attempts} => {lastname}, {start_page}, {debugging}"
+                )
                 if start_page == debugging:
                     attempts += 1
                 else:
@@ -91,14 +95,22 @@ class WebScraper:
         self.close_driver()
 
     def scrape_table(self, data_array):
-        table = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#mainContent > div > div.table-responsive > table")))
+        table = self.wait.until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "#mainContent > div > div.table-responsive > table")
+            )
+        )
         rows = table.find_elements(By.CSS_SELECTOR, "tbody tr")
         for row in rows:
             cells = row.find_elements(By.CSS_SELECTOR, "td")
             row_data = []
             for cell in cells:
                 # Check if the cell contains an <a> element
-                a_tag = cell.find_element(By.CSS_SELECTOR, "a") if cell.find_elements(By.CSS_SELECTOR, "a") else None
+                a_tag = (
+                    cell.find_element(By.CSS_SELECTOR, "a")
+                    if cell.find_elements(By.CSS_SELECTOR, "a")
+                    else None
+                )
                 if a_tag:
                     row_data.append(a_tag.text.strip())
                     url = a_tag.get_attribute("href")
