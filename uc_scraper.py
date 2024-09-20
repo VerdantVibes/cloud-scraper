502b163012c0d580b22661ee1828c169d612d2bc
VerdantVibes <biletskyiivan423@gmail.com>
2024-09-20 05:54:33 +0200

    feat: refact uc_scraper

--- uc_scraper.py
+++ uc_scraper.py
@@ -1,8 +1,8 @@
-from selenium.webdriver.support.ui import WebDriverWait
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-from undetected_chromedriver.options import ChromeOptions
 from selenium.webdriver.support import expected_conditions as EC
+from undetected_chromedriver.options import ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
 import shutil
 import time
 import os
